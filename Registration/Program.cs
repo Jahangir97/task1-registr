47e6569f0a691c5d2ba8b13b1d16c4b848922683
Jahanda97 <jahangirjn@code.edu.az>
2022-05-04 13:38:39 +0400

    first commit

---
+++ Registration/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Registr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Enter Username:");
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter Password:");
+            string password = Console.ReadLine();
+            Admin admin = new Admin(username, password, true, "Section A");
+            Console.WriteLine($"\nSuper admin: {admin.IsSuperAdmin}\nSection: {admin.Section}");
+        }
+    }
+}
---
+++ Registration/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Registr
+{
+    class User
+    {
+        string _username;
+        string _password;
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                if (value.Length > 6)
+                {
+                    _username = value;
+                }
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (value.ToLower() != value)
+                {
+                    _password = value;
+                }
+                else
+                {
+                    Console.WriteLine("Passwordda set olunan zaman maks 1 herf boyuk olmalidir");
+                }
+            }
+        }
+    }
+}
